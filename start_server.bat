eac653a3148c88945ed1cbc55776dbc29633dd7e
alic168168 <alic168168@gmail.com>
2025-12-16 23:56:18 +0800

    Add files via upload

--- start_server.bat
+++ start_server.bat
@@ -4,5 +4,5 @@ echo Please ensure your phone is on the same Wi-Fi.
 echo.
 echo Address to type on phone: http://[YOUR_IP]:8080
 echo.
-call npx http-server -c-1
+call npx http-server -c-1 -o
 pause
